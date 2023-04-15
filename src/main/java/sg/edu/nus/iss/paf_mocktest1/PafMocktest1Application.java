d5f57dc9975f7b5077c46dc7fdadb2747cd4eac5
nchinling <nchinling@gmail.com>
2023-04-15 18:12:30 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/paf_mocktest1/PafMocktest1Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_mocktest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafMocktest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafMocktest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf_mocktest1/PafMocktest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_mocktest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafMocktest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
